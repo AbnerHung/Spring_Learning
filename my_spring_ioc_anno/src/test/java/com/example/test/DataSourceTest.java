e54014d6f108fc81681c6cacf219de2ad3bd1504
めぐみん <59683877+AbnerHung@users.noreply.github.com>
2021-10-19 22:06:23 +0800

    Add files via upload

---
+++ my_spring_ioc_anno/src/test/java/com/example/test/DataSourceTest.java
@@ -0,0 +1,61 @@
+package com.example.test;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.alibaba.druid.pool.DruidPooledConnection;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class DataSourceTest {
+
+    @Test
+    //测试手动创建druid数据源
+    public void test1() throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/db2?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("112358");
+        dataSource.setInitialSize(5);
+        dataSource.setMaxActive(10);
+        dataSource.setMaxWait(3000l);
+        DruidPooledConnection conn = dataSource.getConnection();
+        System.out.println(conn);
+        conn.close();
+    }
+
+    @Test
+    //配置文件创建
+    public void test2() throws Exception {
+        // ResourceBundle rb = ResourceBundle.getBundle("druid");
+        Properties pro  = new Properties();
+        InputStream is = DataSourceTest.class.getClassLoader().getResourceAsStream("druid.properties");
+        pro.load(is);
+        //4.获取连接池对象
+        DataSource ds = DruidDataSourceFactory.createDataSource(pro);
+        //5.获取连接
+        Connection conn = ds.getConnection();
+
+        System.out.println(conn);
+        conn.close();
+    }
+
+    @Test
+    //Spring容器产生数据源对象
+    public void test3() throws SQLException {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DruidDataSource dataSource = (DruidDataSource) app.getBean("dataSource");
+        DruidPooledConnection conn = dataSource.getConnection();
+        System.out.println(conn);
+        conn.close();
+    }
+}

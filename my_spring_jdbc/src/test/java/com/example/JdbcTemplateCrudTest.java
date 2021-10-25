844201e70b657adae60a8197476abafc232379cf
めぐみん <59683877+AbnerHung@users.noreply.github.com>
2021-10-25 22:45:00 +0900

    Update JdbcTemplateCrudTest.java

--- my_spring_jdbc/src/test/java/com/example/JdbcTemplateCrudTest.java
+++ my_spring_jdbc/src/test/java/com/example/JdbcTemplateCrudTest.java
@@ -1,12 +1,16 @@
 package com.example;
 
 import com.example.config.SpringConfiguration;
+import com.example.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {SpringConfiguration.class})
@@ -23,5 +27,11 @@ public class JdbcTemplateCrudTest {
     @Test
     public void testDelete(){
         jdbcTemplate.update("delete from db2.`account` where id = ?",7);
+    }
+
+    @Test
+    public void testQueryAll(){
+        List<Account> rs = jdbcTemplate.query("select * from db2.`account`", new BeanPropertyRowMapper<Account>(Account.class));
+        System.out.println(rs);
     }
 }
